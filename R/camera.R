65a50f0d83cb023286cfb97cfddc69e625a90a6e
JohnCoene <jcoenep@gmail.com>
2019-10-25 22:15:32 +0200

    camera position function added

--- R/camera.R
+++ R/camera.R
@@ -27,7 +27,7 @@
 #'       graph() %>% 
 #'       graph_live_layout(time_step = 5)
 #'   })
-#' 
+#' r
 #'   observeEvent(input$fov, {
 #'     graph_proxy("graph") %>% 
 #'       graph_camera_fov(input$fov)
@@ -48,5 +48,63 @@ graph_camera_fov.graph_proxy <- function(g, fov = 75L) {
 
   g$session$sendCustomMessage("camera-fov", msg)
   
+  return(g)
+}
+
+#' Camera Position
+#' 
+#' Position the camera: note that this only works on a stable graph,
+#' see \code{\link{graph_stable_layout}}.
+#' 
+#' @inheritParams graph_nodes
+#' @param x,y,z Coordinates of camera position.
+#' 
+#' @examples 
+#' library(shiny)
+#' 
+#' gdata <- make_data(100)
+#' 
+#' ui <- fluidPage(
+#'   actionButton("mv", "Move camera"),
+#'   graphOutput("g", height = "80vh")
+#' )
+#' 
+#' server <- function(input, output) {
+#'   output$g <- render_graph({
+#'     graph(gdata) %>% 
+#'       graph_stable_layout(ms = 5000)
+#'   })
+#' 
+#'   observeEvent(input$mv, {
+#'     graph_proxy("g") %>% 
+#'       graph_camera_position(
+#'         sample(1:500, 1),
+#'         sample(1:500, 1),
+#'         sample(1:500, 1)
+#'       )
+#'   })
+#' }
+#' 
+#' if(interactive()) shinyApp(ui, server)
+#' 
+#' @export
+graph_camera_position <- function(g, x = NULL, y = NULL, z = NULL) UseMethod("graph_camera_position")
+
+#' @export
+#' @method graph_camera_position graph
+graph_camera_position.graph <- function(g, x = NULL, y = NULL, z = NULL){
+
+  g$x$camera <- list(x = x, y = y, z = z)
+
+  return(g)
+}
+
+#' @export
+#' @method graph_camera_position graph_proxy
+graph_camera_position.graph_proxy <- function(g, x = NULL, y = NULL, z = NULL){
+
+  msg <- list(id = g$id, x = x, y = y, z = z)
+  g$session$sendCustomMessage("position-camera", msg)
+
   return(g)
 }
